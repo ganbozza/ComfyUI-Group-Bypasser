8564dc29f098f2942c96ab9dc63d3f6dd7b6d2a8
ganbozza <ganbozza@gmail.com>
2026-07-28 03:12:43 +0800

    Update group_bypasser.js

--- web/group_bypasser.js
+++ web/group_bypasser.js
@@ -333,7 +333,7 @@ function syncWidgets(node, groupsByTitle, stateStore) {
     {
        for(const ag of entry.alt_groups)
        {
-          stateStore[keyForTitle(ag)] = targetBypassed;
+          stateStore[keyForTitle(ag)] = !targetBypassed;
        }
     }
   }
