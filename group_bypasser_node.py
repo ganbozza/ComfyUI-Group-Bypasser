0bfeae4d153a66ba01df3de20177ad6122264ce7
ganbozza <ganbozza@gmail.com>
2026-07-29 23:15:36 +0800

    Add boolean input to ComfyUIGroupBypasser
    Added a boolean input to the ComfyUIGroupBypasser class and modified the noop method to return the boolean value.

--- group_bypasser_node.py
+++ group_bypasser_node.py
@@ -1,15 +1,18 @@
 class ComfyUIGroupBypasser:
     @classmethod
     def INPUT_TYPES(cls):
-        return {"required": {}}
+        return {"required": {
+            # Creates a boolean widget/input (True/False toggle)
+                "my_bool": ("BOOLEAN", {"default": False, "force_input": False}),
+        }}
 
-    RETURN_TYPES = ()
+    RETURN_TYPES = ("BOOLEAN",)
     RETURN_NAMES = ()
     FUNCTION = "noop"
     CATEGORY = "utils"
 
-    def noop(self):
-        return ()
+    def noop(self, my_bool):
+        return (my_bool,)
 
 
 NODE_CLASS_MAPPINGS = {
